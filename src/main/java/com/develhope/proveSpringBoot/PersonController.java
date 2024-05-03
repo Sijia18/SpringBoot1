1903a7e1e5fef05f1e91c27bed79a16bef222226
Aurora Scalici <aurorascalici1899@icloud.com>
2024-05-03 12:38:26 +0200

    new Push

--- src/main/java/com/develhope/proveSpringBoot/PersonController.java
+++ src/main/java/com/develhope/proveSpringBoot/PersonController.java
@@ -1,15 +1,14 @@
 package com.develhope.proveSpringBoot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
 
-    @GetMapping (path = "/v1/{provincia}")
-    public Person presentation(@RequestParam String nome, @PathVariable String provincia) {
-        return new Person(nome, provincia);
+    @GetMapping("/v1/ciao")
+    public String presentation(@RequestParam String nome, @RequestParam String provincia) {
+        return "Ciao " + nome + ", com'è il tempo in " + provincia + "?";
     }
 }
